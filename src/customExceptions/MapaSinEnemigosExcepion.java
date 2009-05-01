d745b55aee0572f1e2bb424622cac08c8cb4c6c5
exustheone <ExusTheOne@gmail.com>
2009-05-01 18:42:21 +0000

    Me olvide de las Excepciones en el anterior :P

---
+++ src/customExceptions/MapaSinEnemigosExcepion.java
@@ -0,0 +1,10 @@
+package customExceptions;
+
+public class MapaSinEnemigosExcepion extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2192141258995960585L;
+
+}
---
+++ src/customExceptions/NoteHagaselVivoException.java
@@ -0,0 +1,10 @@
+package customExceptions;
+
+public class NoteHagaselVivoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1120473670654976907L;
+
+}
