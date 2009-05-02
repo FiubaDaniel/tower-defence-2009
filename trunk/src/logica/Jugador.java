c761582a59d2ef37f283b2713f0e05793dc1da2d
ExusTheOne@gmail.com <ExusTheOne@gmail.com@2aa545ca-345c-11de-9d05-31133e6853b1>
2009-05-02 01:36:23 +0000

    Cambie mis clases para que funcionen con la modificación de las excepciones. Arregle un par de errores en escenario.

--- trunk/src/logica/Jugador.java
+++ trunk/src/logica/Jugador.java
@@ -1,9 +1,6 @@
 package logica;
 
-import javax.management.InvalidAttributeValueException;
-
 import customExceptions.DineroMuyBajoException;
-import customExceptions.VidaMenorCantidadException;
 
 public class Jugador {
 
@@ -13,15 +10,13 @@ public class Jugador {
 	private String Nombre;
 	private double Puntos;
 
-	public static Jugador crearJugador(int vida, double dinero, String nombre)
-			throws InvalidAttributeValueException {
+	public static Jugador obtenerJugador(int vida, double dinero, String nombre){
 		if (jugador == null)
 			jugador = new Jugador(vida, dinero, nombre);
 		return jugador;
 	}
 
-	private Jugador(int vida, double dinero, String nombre)
-			throws InvalidAttributeValueException {
+	private Jugador(int vida, double dinero, String nombre) {
 
 		if ((vida > 0) && (dinero > 0)) {
 			this.setCantidadVidas(vida);
@@ -29,85 +24,71 @@ public class Jugador {
 			this.setNombre(nombre);
 			this.setPuntos(0);
 		} else
-			throw new InvalidAttributeValueException();
-
+			throw new IllegalArgumentException();
 	}
 
-	private void setCantidadVidas(int cantidadVidas)
-			throws InvalidAttributeValueException {
-		if (cantidadVidas >= 0)
+	private void setCantidadVidas(int cantidadVidas) {
 			this.CantidadVidas = cantidadVidas;
-		else
-			throw new InvalidAttributeValueException();
 	}
 
 	public int getCantidadVidas() {
 		return CantidadVidas;
 	}
-
-	private void setDinero(double dinero) throws InvalidAttributeValueException {
+	private void setDinero(double dinero){
 		if (dinero >= 0)
 			this.Dinero = dinero;
 		else
-			throw new InvalidAttributeValueException();
+			throw new IllegalArgumentException();
 	}
 
 	public double getDinero() {
 		return Dinero;
 	}
 
-	public void setNombre(String nombre) throws InvalidAttributeValueException {
+	public void setNombre(String nombre) {
 		if (!nombre.isEmpty())
 			this.Nombre = nombre;
 		else
-			throw new InvalidAttributeValueException();
+			throw new IllegalArgumentException();
 	}
 
 	public String getNombre() {
 		return Nombre;
 	}
 
-	private void setPuntos(double puntos) throws InvalidAttributeValueException {
+	private void setPuntos(double puntos) {
 		if (puntos >= 0)
 			this.Puntos = puntos;
 		else
-			throw new InvalidAttributeValueException();
+			throw new IllegalArgumentException();
 	}
 
 	public double getPuntos() {
 		return Puntos;
 	}
 
-	public void AgregarQuitarVida(int cantidad)
-			throws VidaMenorCantidadException {
+	public void AgregarQuitarVida(int cantidad) {
 		// recibe un valor positivo o negativo según quiera agregar vida o
 		// quitar vida
 
-		try {
 			this.setCantidadVidas(CantidadVidas + cantidad);
-		} catch (InvalidAttributeValueException e) {
-			throw new VidaMenorCantidadException();
-
-		}
-
 	}
 
 	public void ModificarDinero(double dinero) throws DineroMuyBajoException {
 		// recibe un valor positivo si agrego dinero y negativo si quito dinero
 		try {
 			this.setDinero(Dinero + dinero);
-		} catch (InvalidAttributeValueException e) {
+		} catch (IllegalArgumentException e) {
 			throw new DineroMuyBajoException();
 		}
 	}
 
-	public void agregarPuntos(double puntos)
-			throws InvalidAttributeValueException {
+	public void agregarPuntos(double puntos) {
 
 		if (puntos >= 0) {
 			this.setPuntos(Puntos + puntos);
 		} else
-			throw new InvalidAttributeValueException();
+			throw new IllegalArgumentException();
 
 	}
 }
