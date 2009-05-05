cb767bb0877065a8e23738f60a5d8cd5ab2fc794
FiubaDaniel <FiubaDaniel@2aa545ca-345c-11de-9d05-31133e6853b1>
2009-05-05 23:17:58 +0000

    al fin pude subir algo

---
+++ src/logica/TorreDorada.java
@@ -0,0 +1,38 @@
+package logica;
+
+import customExceptions.DineroMuyBajoException;
+import logica.Enemigo;
+import logica.Jugador;
+import logica.Posicion;
+
+public class TorreDorada extends Torre{
+	
+	/*instancio un jugador*/
+	private Jugador jugador= Jugador.obtenerJugador(1,60,"Daniel");
+	
+	/*constructor*/
+	public TorreDorada(Posicion posicionEnEscenario){
+	     if(this.getCosto()>=jugador.getDinero()){
+	    	 throw new DineroMuyBajoException();
+	     }
+	     else{
+	    	 	this.setPosicion(posicionEnEscenario);	 	
+	     }
+	     this.setAlcance(7);
+	     this.setCosto(50);
+	     this.setDaño(4);
+	     this.setCostoEvolucion(25);
+	     this.setEvolucion(6);
+	}
+	
+	public void evolucionarce(){	
+	    /*nombre aca tambien*/
+		if (jugador.getDinero()>getCostoEvolucion()){
+			this.setAlcance(this.getAlcance()+this.getEvolucion());
+			this.setCostoEvolucion(this.getCostoEvolucion()+this.getEvolucion());
+		}	
+			else{
+				throw new DineroMuyBajoException();
+			}
+		}
+}
