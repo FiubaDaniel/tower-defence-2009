371daa99964faaf8789c1449f0b2dccf22435666
exustheone@gmail.com <exustheone@gmail.com@2aa545ca-345c-11de-9d05-31133e6853b1>
2009-05-22 23:09:20 +0000

    Les mando la clase escenario con: Métodos para insertar enemigos y obstáculos(estos verifican si los querés meter en una posición válida o no, y para variar tiran una excepción). Un método que devuelve un iterador de obstaculos. Y un método que recibe una posición y te dice si es caminable o no.

---
+++ trunk/src/customExceptions/InvalidPositionException.java
@@ -0,0 +1,11 @@
+package customExceptions;
+
+/**
+ *
+ * @author exus
+ */
+public class InvalidPositionException extends RuntimeException {
+    
+    private static final long serialVersionUID = -35233234329237263L;
+
+}
