fac74e7d502d1633d128fdfc21856d9383501b9c
exustheone <ExusTheOne@gmail.com>
2009-05-24 17:43:35 +0000

    Le cambie el formato de los mapas, ahora son de 50 x 50. Simplemente para que se vea mejorcita la parte gráfica. Los "tiles" de la ventana para dibujar, tienen que ser de 15 x 15 pixels.

--- src/vista/menu/PanelDatosDeSeleccion.java
+++ src/vista/menu/PanelDatosDeSeleccion.java
@@ -2,7 +2,6 @@ package vista.menu;
 
 import javax.swing.JPanel;
 
-
 public class PanelDatosDeSeleccion extends JPanel {
 
 	/**
@@ -28,10 +27,9 @@ public class PanelDatosDeSeleccion extends JPanel {
 		LinkVida_danio = new javax.swing.JLabel();
 		LinkRango_Vel = new javax.swing.JLabel();
 		ButtonUpdate = new BotonUpdate();
-		
 
-		setBorder(javax.swing.BorderFactory
-				.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 0, 0)));
+		setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1,
+				new java.awt.Color(255, 0, 0)));
 
 		ImagenSeleccion.setBorder(javax.swing.BorderFactory
 				.createTitledBorder(""));
@@ -58,53 +56,153 @@ public class PanelDatosDeSeleccion extends JPanel {
 
 		LinkRango_Vel.setText("Rango_Vel");
 
-		
-
 		javax.swing.GroupLayout PanelDatosSeleccionLayout = new javax.swing.GroupLayout(
 				this);
 		setLayout(PanelDatosSeleccionLayout);
-		PanelDatosSeleccionLayout.setHorizontalGroup(PanelDatosSeleccionLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-			PanelDatosSeleccionLayout.createSequentialGroup().addContainerGap()
-			.addGroup(PanelDatosSeleccionLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.LEADING).addComponent(LabelNombreSeleccion,
-			javax.swing.GroupLayout.PREFERRED_SIZE,118,javax.swing.GroupLayout.PREFERRED_SIZE)
-			.addGroup(PanelDatosSeleccionLayout.createSequentialGroup().addGap(6, 6, 6)
-			.addComponent(ImagenSeleccion,javax.swing.GroupLayout.PREFERRED_SIZE,
-			javax.swing.GroupLayout.DEFAULT_SIZE,javax.swing.GroupLayout.PREFERRED_SIZE)
-			.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-			.addGroup(PanelDatosSeleccionLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.LEADING).addGroup(PanelDatosSeleccionLayout
-			.createSequentialGroup().addGroup(PanelDatosSeleccionLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.LEADING).addComponent(LabelVida_danio)
-			.addComponent(LabelRango_Vel)).addGap(29, 29, 29).addGroup(PanelDatosSeleccionLayout
-			.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING).addComponent(
-			LinkRango_Vel).addComponent(LinkVida_danio)).addGap(35, 35, 35)).addGroup(
-			PanelDatosSeleccionLayout.createSequentialGroup().addComponent(LabelUpdate)
-			.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-			.addComponent(ButtonUpdate))).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
-			.addContainerGap(26, Short.MAX_VALUE)));
-		
-		PanelDatosSeleccionLayout.setVerticalGroup(PanelDatosSeleccionLayout
-			.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-			.addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
-			PanelDatosSeleccionLayout.createSequentialGroup().addContainerGap()
-			.addComponent(LabelNombreSeleccion).addGroup(PanelDatosSeleccionLayout
-			.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-			javax.swing.GroupLayout.Alignment.TRAILING,PanelDatosSeleccionLayout
-			.createSequentialGroup().addGap(6, 6, 6).addGroup(PanelDatosSeleccionLayout
-			.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE).addComponent(
-			LabelVida_danio).addComponent(LinkVida_danio)).addPreferredGap(
-			javax.swing.LayoutStyle.ComponentPlacement.RELATED,javax.swing.GroupLayout.DEFAULT_SIZE,
-			Short.MAX_VALUE).addGroup(PanelDatosSeleccionLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.BASELINE).addComponent(LabelRango_Vel)
-			.addComponent(LinkRango_Vel)).addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-			.addGroup(PanelDatosSeleccionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-			.addComponent(LabelUpdate).addComponent(ButtonUpdate)).addGap(38, 38, 38))
-			.addGroup(PanelDatosSeleccionLayout.createSequentialGroup().addGap(18, 18, 18)
-			.addComponent(ImagenSeleccion,javax.swing.GroupLayout.PREFERRED_SIZE,
-			javax.swing.GroupLayout.DEFAULT_SIZE,javax.swing.GroupLayout.PREFERRED_SIZE)))
-			.addContainerGap()));
+		PanelDatosSeleccionLayout
+				.setHorizontalGroup(PanelDatosSeleccionLayout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								PanelDatosSeleccionLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addGroup(
+												PanelDatosSeleccionLayout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING)
+														.addComponent(
+																LabelNombreSeleccion,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																118,
+																javax.swing.GroupLayout.PREFERRED_SIZE)
+														.addGroup(
+																PanelDatosSeleccionLayout
+																		.createSequentialGroup()
+																		.addGap(
+																				6,
+																				6,
+																				6)
+																		.addComponent(
+																				ImagenSeleccion,
+																				javax.swing.GroupLayout.PREFERRED_SIZE,
+																				javax.swing.GroupLayout.DEFAULT_SIZE,
+																				javax.swing.GroupLayout.PREFERRED_SIZE)
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																		.addGroup(
+																				PanelDatosSeleccionLayout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.LEADING)
+																						.addGroup(
+																								PanelDatosSeleccionLayout
+																										.createSequentialGroup()
+																										.addGroup(
+																												PanelDatosSeleccionLayout
+																														.createParallelGroup(
+																																javax.swing.GroupLayout.Alignment.LEADING)
+																														.addComponent(
+																																LabelVida_danio)
+																														.addComponent(
+																																LabelRango_Vel))
+																										.addGap(
+																												29,
+																												29,
+																												29)
+																										.addGroup(
+																												PanelDatosSeleccionLayout
+																														.createParallelGroup(
+																																javax.swing.GroupLayout.Alignment.TRAILING)
+																														.addComponent(
+																																LinkRango_Vel)
+																														.addComponent(
+																																LinkVida_danio))
+																										.addGap(
+																												35,
+																												35,
+																												35))
+																						.addGroup(
+																								PanelDatosSeleccionLayout
+																										.createSequentialGroup()
+																										.addComponent(
+																												LabelUpdate)
+																										.addPreferredGap(
+																												javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																										.addComponent(
+																												ButtonUpdate)))
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+										.addContainerGap(26, Short.MAX_VALUE)));
+
+		PanelDatosSeleccionLayout
+				.setVerticalGroup(PanelDatosSeleccionLayout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								javax.swing.GroupLayout.Alignment.TRAILING,
+								PanelDatosSeleccionLayout
+										.createSequentialGroup()
+										.addContainerGap()
+										.addComponent(LabelNombreSeleccion)
+										.addGroup(
+												PanelDatosSeleccionLayout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING)
+														.addGroup(
+																javax.swing.GroupLayout.Alignment.TRAILING,
+																PanelDatosSeleccionLayout
+																		.createSequentialGroup()
+																		.addGap(
+																				6,
+																				6,
+																				6)
+																		.addGroup(
+																				PanelDatosSeleccionLayout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.BASELINE)
+																						.addComponent(
+																								LabelVida_danio)
+																						.addComponent(
+																								LinkVida_danio))
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+																				javax.swing.GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addGroup(
+																				PanelDatosSeleccionLayout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.BASELINE)
+																						.addComponent(
+																								LabelRango_Vel)
+																						.addComponent(
+																								LinkRango_Vel))
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																		.addGroup(
+																				PanelDatosSeleccionLayout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.BASELINE)
+																						.addComponent(
+																								LabelUpdate)
+																						.addComponent(
+																								ButtonUpdate))
+																		.addGap(
+																				38,
+																				38,
+																				38))
+														.addGroup(
+																PanelDatosSeleccionLayout
+																		.createSequentialGroup()
+																		.addGap(
+																				18,
+																				18,
+																				18)
+																		.addComponent(
+																				ImagenSeleccion,
+																				javax.swing.GroupLayout.PREFERRED_SIZE,
+																				javax.swing.GroupLayout.DEFAULT_SIZE,
+																				javax.swing.GroupLayout.PREFERRED_SIZE)))
+										.addContainerGap()));
 
 	}
 
